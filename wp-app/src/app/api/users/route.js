19c644dd0f794f3107169750bcb24d58d499f06c
= <itfaria@gmail.com>
2023-07-08 20:44:11 +0100

    OMG

--- wp-app/src/app/api/users/route.js
+++ wp-app/src/app/api/users/route.js
@@ -1,7 +1,22 @@
 import { NextResponse } from 'next/server'
 import Users from '../../components/Users'
 
-export async function GET(req) {
-    const users = Users()
+export async function GET({ users }) {
     return NextResponse.json(users)
 }
+
+export async function getStaticPaths() {
+    // Return empty paths because we don't want to generate anything on build
+    // { fallback: blocking } will server-render pages
+    // on-demand if the path doesn't exist.
+    return {
+      paths: [],
+      fallback: 'blocking',
+    };
+  }
+
+  export const getStaticProps = async () => {
+    const users = Users()
+    return { props: { users } }
+  }
+
