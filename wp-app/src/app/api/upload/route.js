b71a9c3969f15c71f6e73017c89462a2e293fab2
= <itfaria@gmail.com>
2023-07-13 17:37:27 +0100

    Vercel complaining about serveless function size

--- wp-app/src/app/api/upload/route.js
+++ wp-app/src/app/api/upload/route.js
@@ -2,21 +2,22 @@ import { NextResponse } from 'next/server'
 import Image from '@/app/components/Image'
 
 export async function POST(req) {
-  let response = []
   const formData = await req.formData()
   const formDataEntryValues = Array.from(formData.values())
   let image = new Image()
   for (const formDataEntryValue of formDataEntryValues) {
       if (typeof formDataEntryValue === 'object' && 'arrayBuffer' in formDataEntryValue) {
-
+        
         image = await image.save(formDataEntryValue)
+        /*
         if('filepath' in image){
           response.push(image.filepath)
           response.player = image.player
           window.image = image
         }
       console.log('Upload response', response)
+      */
     }
   }
-  return NextResponse.json({ response: response })
+  return NextResponse.json({})
 }
